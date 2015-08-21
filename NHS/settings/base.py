8e182870a9d500bf2fe5c0c5e5ac527c3c046a33
somilgo <somilgovani@gmail.com>
2015-08-21 19:29:38 -0400

    static fix pt 2

--- NHS/settings/base.py
+++ NHS/settings/base.py
@@ -117,5 +117,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+
 )
--- NHS/settings/production.py
+++ NHS/settings/production.py
@@ -22,5 +22,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+
 )
