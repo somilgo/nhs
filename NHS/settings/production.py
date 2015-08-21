ca9d53166a6b5a3f68aa70681f06bf4d74013918
somilgo <somilgovani@gmail.com>
2015-08-21 19:50:55 -0400

    mo

--- NHS/settings/production.py
+++ NHS/settings/production.py
@@ -2,7 +2,7 @@ from django.conf import settings
 
 DEBUG = False
 TEMPLATE_DEBUG = True
-
+DATABASES = settings.DATABASES
 
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
