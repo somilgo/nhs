5fb3d27e3e968d4e00a10118580005cdfcd225d4
somilgo <somilgovani@gmail.com>
2015-08-21 18:48:54 -0400

    add requirements

--- NHS/wsgi.py
+++ NHS/wsgi.py
@@ -14,3 +14,12 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "NHS.settings")
 
 application = get_wsgi_application()
+
+try:
+
+	from dj_static import Cling
+
+	application = Cling(get_wsgi_application())
+
+except:
+	pass
