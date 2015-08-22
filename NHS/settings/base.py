41271372578de2e34fd9763855b9ad7f430ef1cb
somilgo <somilgovani@gmail.com>
2015-08-21 20:18:13 -0400

    money

--- NHS/settings/base.py
+++ NHS/settings/base.py
@@ -112,10 +112,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
-
-STATICFILES_DIRS = (
-
-)
