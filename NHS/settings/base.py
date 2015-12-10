1818b6dc3975bfe1447d71e501ca0cf39af8f8c4
somilgo <somilgovani@gmail.com>
2015-12-10 17:02:15 -0500

    january

--- NHS/settings/base.py
+++ NHS/settings/base.py
@@ -30,7 +30,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '=6b_2=!3s2hq3-nc@#rx6v=##u53xt!b=(#)c(2nk%&4qfpvy)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
